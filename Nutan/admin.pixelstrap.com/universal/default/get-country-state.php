73059fccd025b8a453e2c827bf3142f8a3376ecd
samarth8481 <59821956+samarth8481@users.noreply.github.com>
2020-02-07 15:53:57 +0530

    Update get-country-state.php

--- Nutan/admin.pixelstrap.com/universal/default/get-country-state.php
+++ Nutan/admin.pixelstrap.com/universal/default/get-country-state.php
@@ -2,15 +2,18 @@
 include_once("config.php");
     
 
-    $costId = $_POST['switch_name'];
-        
-
-        $cost_query = "SELECT switch_rate from switches where s_id = '".$costId."'";
+    $rate= $_POST['s_id'];        
+//echo $rate;
+//exit;
+        $cost_query = "SELECT switch_rate from switches where s_id = '".$rate."'";
+        //echo $cost_query;exit;
         $cost_query_process = mysqli_query($link,$cost_query);
+        //var_dump($cost_query_process);
+        //exit;
         if (mysqli_num_rows($cost_query_process)>0) {
             while ($row1 = mysqli_fetch_assoc($cost_query_process)) {
         ?>
-           <input type="text" name="rate" value="<?php $row1['switch_rate'];?>">
+           <input type="text" name="rate" onchange="getText3()" id="rate" value="<?php echo $row1['switch_rate'];?>" style="width: 120px;" >
         
 <?php }}?>
             
