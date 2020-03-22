14c883e35ebe17c2974a0c88a466be64c6ef7fcf
samarth8481 <59821956+samarth8481@users.noreply.github.com>
2020-03-22 12:31:57 +0530

    Add files via upload

--- Nutan/admin.pixelstrap.com/universal/default/Create_project.php
+++ Nutan/admin.pixelstrap.com/universal/default/Create_project.php
@@ -1,36 +1,8 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
-<?php
-// Initialize the session
-
-// Include config file
-require_once "config.php";
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-        if (empty($_POST['project_name'])  || empty($_POST['project_description']) ) {
-
-            $error = true;
-        } 
-        else {
-
-            $project_name = $_POST['project_name'];
-            $project_description = $_POST['project_description'];
-
-            $sql = "INSERT INTO project (project_name, project_description) VALUES('$project_name','$project_description')" ;
-                $process_query = mysqli_query($link,$sql);
-                if(!$process_query){
-                    echo $mysqli->error;
-                    }
-                    else{
-                    echo"<script>alert('Project Added Successfully ..!');
-                    window.location.href='index.php';
-                    </script>";
-                }
-                
-        }
-    }
-    ?>
-<!-- Mirrored from admin.pixelstrap.com/universal/default/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 30 Dec 2019 10:51:20 GMT -->
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -72,21 +44,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 
     <!-- Responsive css -->
     <link rel="stylesheet" type="text/css" href="../assets/css/responsive.css">
+        <!-- breadcrum -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/breadcrum.css">
 
 </head>
 <body>
-
-<!-- Loader starts -->
-<!-- <div class="loader-wrapper">
-    <div class="loader bg-white">
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-        <h4>Have a great day at work today <span>&#x263A;</span></h4>
-    </div>
-</div>
- --><!-- Loader ends -->
 
 <!--page-wrapper Start-->
 <div class="page-wrapper">
@@ -96,10 +58,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         <div class="main-header-left">
             <div class="logo-wrapper">
                 <a href="index.php">
-                    <img src="../assets/images/final_logo1.jpg" class="image-dark" alt=""/>
+                   <img src="../assets/images/final_logo2.png" style="height: 56px;" class="image-dark" alt=""/>
+
                 </a>
             </div>
         </div>
+        <ul class="breadcrumb">
+  <li><a href="index.php">Home</a></li>
+
+  <li>Create project</li>
+</ul>
         <div class="main-header-right row">
             <div class="nav-right col">
                 <ul class="nav-menus">
@@ -118,38 +86,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
                             <img class="align-self-center pull-right mr-2" src="../assets/images/dashboard/user.png" alt="header-user"/>
                             <div class="media-body">
                                 <h6 class="m-0 txt-dark f-16">
-                                    My Account
+                                     <?php echo $_SESSION['username']; ?>
                                     <i class="fa fa-angle-down pull-right ml-2"></i>
                                 </h6>
                             </div>
                         </div>
                         <ul class="profile-dropdown onhover-show-div p-20">
                             <li>
-                                <a href="#">
+                                <a href="reset-password.php">
                                     <i class="icon-user"></i>
                                     Edit Profile
                                 </a>
                             </li>
                             <li>
-                                <a href="#">
-                                    <i class="icon-email"></i>
-                                    Inbox
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <i class="icon-check-box"></i>
-                                    Task
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <i class="icon-comments"></i>
-                                    Chat
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
+                                <a href="logout.php">
                                     <i class="icon-power-off"></i>
                                     Logout
                                 </a>
@@ -172,13 +122,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             <ul class="sidebar-menu">
                 <li class="active">
                     <div class="sidebar-title">Dashboard</div>
-                    <a href="Create_project.php" class="sidebar-header">
-                        <i class="icon-desktop"></i><span>Create New Project</span>
+                    <a href="index.php" class="sidebar-header">
+                        <i class="icon-desktop"></i><span>List of project</span>
                     </a>
-                    <a href="register.php" class="sidebar-header">
-                        <i class="icon-desktop"></i><span>Create User</span>
-                    </a>
-                    <a href="#" class="sidebar-header">
+                    <a href="list_receipe.php" class="sidebar-header">
                         <i class="icon-desktop"></i><span>List of Recipes</span>
                     </a>
             <div class="sidebar-widget text-center">
@@ -195,11 +142,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             </div>
         </div>
         
-        <!--Page Sidebar Ends-->
-        <!--Page Sidebar Ends-->
-
-<!-- page Body with CSS -->
-
+        
             <div class="page-body"><br>
         <style>
     * {
@@ -242,23 +185,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             .container {
               border-radius: 20px;
               background-color: #f2f2f2;
-              padding: 60px;
+              padding: 5px;
+            }
+            .col-25 {
+              /*float: left;*/
+              width: 40%;
+              margin-top: 30px;
+              padding-left: 30px;
             }
 
-            .col-25 {
+            .col-75 {
+              /*float: left;*/
+              width: 40%;
+              margin-top: 30px;
+              padding-left: 30px;
+              height: 20px;
+            }
+
+            /*.col-25 {
               float: right;
               width: 40%;
               margin-top: 0px;
               /*padding-left: 30px;*/
-            }
+           /* }
 
             .col-75 {
               float: left;
               width: 75%;
-              margin-top: 15px;
+              margin-top: 5px;
               padding-left: 30px;
             }
-
+*/*/
             /* Clear floats after the columns */
             .row:after {
               content: "";
@@ -275,108 +232,84 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             </style>
     
             <div class="container">
-            <form  method="POST">
+            <form method="POST" action="project_process.php">
             <div class="row">
             <div class="col-25">
             <label for="project_name">Project Name</label>
             </div>
             <div class="col-75">
-            <input type="text" id="Project_Name" name="project_name" placeholder="Project Name.." required="Please Enter Project Name..">
+            <input type="text" id="project_name" name="project_name" placeholder="Project Name" required="Please Enter Project Name..">
             </div>
-            </div>
-            <div class="row">
             <div class="col-25">
-            <label for="project_description">Project Description</label>
+            <label for="date">Date</label>
             </div>
             <div class="col-75">
-            <textarea id="project_description" name="project_description" placeholder="Project Description.." required="Please Enter Details.." style="height:200px"></textarea>
+            <input type="date" style="width:200px;" id="date1" name="Date" required="Please Enter date">
             </div>
+            <div class="col-25">
+            <label for="description">Description</label>
+            </div>
+            <div class="col-75">
+            <textarea id="description" name="description" placeholder="description" required="Please Enter description" style="height:60px"></textarea> 
+            </div>
+            <div class="col-25">
+            <label for="Address">Address</label>
+            </div>
+            <div class="col-75">
+            <textarea id="Address" name="address" placeholder="Address" required="Please Enter Address.." style="height:60px"></textarea>
+            </div>
+            <div class="col-25">
+            <label for="developer">Developer name</label>
+            </div>
+            <div class="col-75">
+            <select name="developer"  style="width: 200px;"required="Please Enter Project Name.." >
+            <option selected disabled >Select developer</option>
+            <?php
+            include_once("config.php");
+            $query = "SELECT * FROM `developer`";
+            $query_process = mysqli_query($link,$query);
+                if(mysqli_num_rows($query_process)>0){
+                    while($result = mysqli_fetch_assoc($query_process)){
+            ?>
+            <option value="<?php echo $result["developer_name"]; ?>"><?php echo $result['developer_name']?> </option>      
+            <?php
+            }}?>
+            </select>             
             </div>
             <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            </div>
             <div class="btn">
-            <input type="submit" value="Submit">
+            <input type="submit" value="Submit" name="submit">
             </div>
             </form>
             </div>
         <!--Page Body Ends-->
 
 </div>
-        <!--Page Body Ends-->
 
-    <!--Page Body Ends-->
 
 </div>
- <footer class="footer-fix">
+
+        </div>
+    </div>
+    <footer class="footer-fix">
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-sm-12 col-md-6 col-xl-6 footer-copyright">
                             <p class="mb-0">Nutan Enterprise © 2019 Created By <a href="http://www.fillion.in/" target="_blank">Fillion Solutions Pvt. Ltd.</a></p>
                         </div>
-                        <!-- <div class="col-sm-12  col-md-6 col-xl-6">
-                            <ul class="footer-links">
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">Privacy</a></li>
-                                <li><a href="#">T&amp;C</a></li>
-                            </ul>
-                        </div> -->
+             
                     </div>
                 </div>
 </footer>
-        </div>
-    </div>
 </div>
-<!--page-wrapper Ends-->
 
-<!-- latest jquery-->
-<script src="../assets/js/jquery-3.2.1.min.js" ></script>
 
-<!-- Bootstrap js-->
-<script src="../assets/js/bootstrap/popper.min.js" ></script>
-<script src="../assets/js/bootstrap/bootstrap.js" ></script>
-
-<!-- Chart JS-->
-<!-- <script src="../assets/js/chart/Chart.min.js"></script> -->
-
-<!-- Morris Chart JS-->
-<!-- <script src="../assets/js/morris-chart/raphael.js"></script>
-<script src="../assets/js/morris-chart/morris.js"></script>
- -->
-<!-- owlcarousel js-->
-<!-- <script src="../assets/js/owlcarousel/owl.carousel.js" ></script> -->
-
-<!-- Sidebar jquery-->
-<!-- <script src="../assets/js/sidebar-menu.js" ></script> -->
-
-<!--Sparkline  Chart JS-->
-<!-- <script src="../assets/js/sparkline/sparkline.js"></script> -->
-
-<!--Height Equal Js-->
-<!-- <script src="../assets/js/height-equal.js"></script> -->
-
-<!-- prism js -->
-<!-- <script src="../assets/js/prism/prism.min.js"></script> -->
-
-<!-- clipboard js -->
-<!-- <script src="../assets/js/clipboard/clipboard.min.js" ></script>
- -->
-<!-- custom card js  -->
-<!-- <script src="../assets/js/custom-card/custom-card.js" ></script>
- -->
-<!-- Theme js-->
-<script src="../assets/js/script.js" ></script>
-<!-- <script src="../assets/js/theme-customizer/customizer.js"></script> -->
-<!-- <script src="../assets/js/chat-sidebar/chat.js"></script> -->
-<script src="../assets/js/dashboard-default.js" ></script>
-
-<!-- Counter js-->
-<!-- <script src="../assets/js/counter/jquery.waypoints.min.js"></script>
-<script src="../assets/js/counter/jquery.counterup.min.js"></script>
-<script src="../assets/js/counter/counter-custom.js"></script>
-
-<script src="../assets/js/notify/bootstrap-notify.min.js"></script>
-<script src="../assets/js/notify/index.js"></script>
- -->
 </body>
-
-<!-- Mirrored from admin.pixelstrap.com/universal/default/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 30 Dec 2019 10:53:16 GMT -->
 </html>
