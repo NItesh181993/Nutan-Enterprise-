9cea8ee73797d23cdf1852b7c65113abd26e5da7
samarth8481 <59821956+samarth8481@users.noreply.github.com>
2020-04-16 11:27:05 +0530

    Update test1_sb.php

--- Nutan/admin.pixelstrap.com/universal/default/test1_sb.php
+++ Nutan/admin.pixelstrap.com/universal/default/test1_sb.php
@@ -15,14 +15,14 @@ $rate=$_POST['rate'];
 $quantity=$_POST['qty'];
 $total=$_POST['total'];
 $gtotal =$_POST['gtotal'];
-   }}
+   
    $query12= "SELECT id FROM `receipe` where receipe_name= '".$receipe_name."'";
    $mysqli_query12= mysqli_query($link,$query12);
        // echo $query1;
         if (mysqli_num_rows($mysqli_query12)>0) {
             while ($row12 = mysqli_fetch_assoc($mysqli_query12)) {
               $id12=$row12['id'];
-              }}                          
+                                        
  $query2 ="INSERT INTO `sb_details`( 
  `sb_id`, 
  `receipe_name`, 
@@ -36,12 +36,12 @@ VALUES('".$id."',
 '".$rate."',
 '".$quantity."',
 '".$total."')";
-echo $query2;
   mysqli_query($link,$query2);
   
-  // echo $id;
- $query3="UPDATE `sb` SET `total`='".$gtotal."', p_id='".$pid."' WHERE id= '".$id."'";
- mysqli_query($link,$query3);
- echo $query3;
+  }
+}
+}}
 
-?>
+ 
+
+?>  
